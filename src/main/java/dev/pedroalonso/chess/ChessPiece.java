1ce218dded254bf1530e84e47d70e8264372635a
Pedro Alonso <phaalonso@gmail.com>
2022-11-26 19:01:33 -0300

    Increase and decrease move count

--- src/main/java/dev/pedroalonso/chess/ChessPiece.java
+++ src/main/java/dev/pedroalonso/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import dev.pedroalonso.boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private final Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -24,5 +25,17 @@ public abstract class ChessPiece extends Piece {
         ChessPiece p = (ChessPiece) getBoard().getPiece(position);
 
         return p != null && p.getColor() != color;
+    }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
     }
 }
